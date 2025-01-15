e096ec78b86d51ff7b26342a43f0f1f7184b5b79
Juno-Whut <owenrupnow17@gmail.com>
2025-01-14 19:58:45 -0500

    Solved problem 1539 in O(n) time and O(1) space, took me like 10 minutes of suboptimal thinking since it was noisy and then coded it up and had to move some stuff aroun overall took me about 20-25 minutes which I think can be improved. There is an extra challenge at the end of this problem that I am doing tomorrow which is 'Can you solve this problem faster than O(n)' and I think you can solve it in O(log n) since there is zero way of you doing it in constant time because of the array, so will be giving that a crack tomorrow

---
+++ EasyProblems/1539_Kth_Missing_Positive_Num.py
@@ -0,0 +1,32 @@
+import unittest
+
+class Solution:
+    def findKthPositive(self, arr, k: int) -> int:
+        missing_nums, expected_num, arr_num = 0, 0, None
+
+        while missing_nums < k:
+            expected_num += 1
+
+            if not arr_num and arr:
+                arr_num = arr.pop(0)
+            
+            if expected_num == arr_num:
+                arr_num = None
+            else:
+                missing_nums += 1
+
+        return expected_num 
+
+class TestfindKthPositive(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(Solution().findKthPositive([2, 3, 4, 7, 11], 5), 9)
+    
+    def test3(self):
+        self.assertEqual(Solution().findKthPositive([1, 2, 3, 4], 2), 6)   
+
+    def test2(self):
+        self.assertEqual(Solution().findKthPositive([2], 1), 1) 
+
+
+if __name__ == '__main__':
+    unittest.main()
