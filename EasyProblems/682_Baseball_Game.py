85f701ee1cf591ebcc2ed869942259f66b08fb22
Juno-whut <owenrupnow17@gmail.com>
2025-01-30 15:47:00 -0500

    It feels good to come back fresh with a different idea even if the idea is only slightly different and it cleans up the code and answers the problem. Solved problem 682 Baseball Game optimally which is O(n) time and space. Heres to getting consitent again

--- EasyProblems/682_Baseball_Game.py
+++ EasyProblems/682_Baseball_Game.py
@@ -4,35 +4,17 @@ import unittest
 class Solution:
     def calPoints(self, operations) -> int:
         stack = []
-        previous, previous2 = 0,0
 
-        while operations:
-            current_opp = operations.pop(0)
-
-            if current_opp.isdigit():
-                stack.append(int(current_opp))
-                current_num = int(current_opp)
-            
-            if current_opp == '+':
-                stack.append((previous + previous2))
-                current_num = previous +previous2
-            
-            if current_opp == 'D':
-                stack.append(previous*2)
-                current_num = previous*2
-
-            if current_opp == 'C':
+        for i in operations:
+            if i == 'C':
                 stack.pop()
-                len_stack = len(stack)
-                if len_stack > 0:
-                    previous = stack[len_stack-1]
-                    previous2 = stack[len_stack-2]
-                else:
-                    previous = 0
-                    previous2 = 0
+            elif i == 'D':
+                stack.append(2 * stack[-1])
+            elif i == '+':
+                stack.append(stack[-1] + stack[-2])
             else:
-                previous2 = previous
-                previous = current_num
+                stack.append(int(i))
+            
         return sum(stack)
 
 
