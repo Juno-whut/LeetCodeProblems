14fb6ab38de2e14082c7b1c4533348f408c8e2ea
Juno-whut <owenrupnow17@gmail.com>
2025-01-31 23:09:24 -0500

    Prepping problem 188 Best Time to buy and Sell Stock 4 file to only have the unittest import, class Solution with def maxProfit with pass inside, and a testsolution class with prepped test cases

--- HardProblems/188_Best_Time_To_Buy_Sell_Stock_4.py
+++ HardProblems/188_Best_Time_To_Buy_Sell_Stock_4.py
@@ -3,38 +3,27 @@ import unittest
 
 class Solution:
     def maxProfit(self, k: int, prices) -> int:
-        currentLow = prices[0]
-        MaxProfit = 0
-        maxSells = k
-        
-        if len(prices) == 1:
-            return 0
-        
-        for i in range(1, len(prices)):
-            if maxSells == 0:
-                return MaxProfit
-            if prices[i] < currentLow:
-                currentLow = prices[i]
-            else:
-                MaxProfit += prices[i] - currentLow
-                currentLow = prices[i]
-                maxSells -= 1
-        
-        return MaxProfit
+        pass
 
 
 
   
 
 class TestMaxProfit(unittest.TestCase):
+    def leet1(self):
+        self.assertEqual(Solution().maxProfit(2, [2,4,1]), 2)
+    
+    def leet2(self):
+        self.assertEqual(Solution().maxProfit(2, [[3,2,6,5,0,3]]), 7)
+        
     def test1(self):
-        self.assertEqual(Solution().maxProfit(2, [3,3,5,0,0,3,1,4]), 6)
+        self.assertEqual(Solution().maxProfit(2, [3,3,5,0,0,3,1,4]), 6) 
     
     def test2(self):
-        self.assertEqual(Solution().maxProfit(2, [1,2,3,4,5]), 4)
+        self.assertEqual(Solution().maxProfit(2, [1,2,3,4,5]), 4) # Stricly increasing
 
     def test3(self):
-        self.assertEqual(Solution().maxProfit(2, [7,6,4,3,1]), 0)
+        self.assertEqual(Solution().maxProfit(2, [7,6,4,3,1]), 0) # Stricly decreasing
 
     def test4(self):
         self.assertEqual(Solution().maxProfit(2, [6,1,3,2,4,7]), 7)
