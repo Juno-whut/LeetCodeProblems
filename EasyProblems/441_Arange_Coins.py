f05853906cf3928dd5426696968a4c3531f262be
Juno-whut <owenrupnow17@gmail.com>
2025-02-13 15:43:22 -0500

    Completed problem 441 in O(log n) time and O(1) space. This problem actually gave me more of mental challenge than I thought it would. Since I started with naive approach and miscalculated the runtime complexity since I didnt take my time, I even wrote a comment in my notebook that said binary search was not nessecary. That comment was both right and wrong because there are 3 approaches to this problem naive, math, and binary search. I didnt sign up for the whole math solution that I dont understand even after looking at it and using the formula, so I stopped at the binary search that I know, understand, and can implement it quickly. I lowkey tried to pick a problem that I looked easy too, but its always the easy problems that have the hardest optimal solutions.

---
+++ EasyProblems/441_Arange_Coins.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        left, right = 0, n
+        while left <= right:
+            k = (left + right) // 2
+            current = k * (k + 1) // 2
+            if current == n:
+                return k
+            if current < n:
+                left = k + 1
+            else:
+                right = k - 1
+        return right
+        
+
+class TestAreangeCoins(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution().arrangeCoins
+
+    def test1(self):
+        self.assertEqual(self.s(5), 2)
+
+    def test2(self):
+        self.assertEqual(self.s(8), 3)
+    
+    def test3(self):
+        self.assertEqual(self.s(1), 1)
+
+    def test4(self):
+        self.assertEqual(self.s(24), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
